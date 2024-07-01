aef9826df7c032a4bedeabd0f81d3ba403114140
Yuri Kuzmichev <kuzmichev@it-tian.ru>
2024-07-01 17:58:39 +0500

    minor fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const pathImg = "./assets/img/";
-  const count = 70;
-  const nameImg = "img-";
-  const swiperWrapper = document.querySelector(".swiper-wrapper");
-  // const gallery = document.querySelector('.gallery');
-  const slides = generateImg(count, pathImg, nameImg);
-  // const slidesGallery = generateImg(count, pathImg, nameImg, 'gallery__item');
-  slides.forEach((slide) => swiperWrapper?.appendChild(slide));
-  // slidesGallery.forEach(slide => gallery?.appendChild(slide));
-  if (!!swiperWrapper) {
+  const settingsImg = {
+    count: 70,
+    pathImg: "./assets/img/",
+    nameImg: "img-",
+  };
+  const swiperImage = document.querySelector(".swiper-image");
+  const slides = generateImg(
+    settingsImg.count,
+    settingsImg.pathImg,
+    settingsImg.nameImg
+  );
+  slides.forEach((slide) => swiperImage?.appendChild(slide));
+
+  if (!!swiperImage) {
+    const swiper = new Swiper(".swiper", {
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+
+  const settingsLogo = {
+    count: 5,
+    pathImg: "./assets/logo/",
+    nameImg: "logo-",
+  };
+  const swiperLogo = document.querySelector(".swiper-logo");
+  const slides2 = generateImg(
+    settingsLogo.count,
+    settingsLogo.pathImg,
+    settingsLogo.nameImg
+  );
+  slides2.forEach((slide) => swiperLogo?.appendChild(slide));
+
+  if (!!swiperLogo) {
     const swiper = new Swiper(".swiper", {
       loop: true,
       pagination: {
